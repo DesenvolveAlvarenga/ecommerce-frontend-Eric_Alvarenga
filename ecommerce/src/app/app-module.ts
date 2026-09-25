a8fea63566c7a8bdbc00f5cd8342d93271d65981
DesenvolveAlvarenga <desenvolvealvarenga@gmail.com>
2026-09-25 01:12:03 +0000

    save

--- ecommerce/src/app/app-module.ts
+++ ecommerce/src/app/app-module.ts
@@ -1,17 +1,14 @@
 import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms'; 
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { Cabecalho } from './cabecalho/cabecalho';
 
 @NgModule({
-  declarations: [App],
-  imports: [
-    BrowserModule, 
-    AppRoutingModule,
-    FormsModule 
-  ],
+  declarations: [App, Cabecalho],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [provideBrowserGlobalErrorListeners()],
   bootstrap: [App],
 })
---
+++ ecommerce/src/app/cabecalho/cabecalho.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cabecalho',
+  standalone: false,
+  templateUrl: './cabecalho.html',
+  styleUrl: './cabecalho.scss',
+})
+export class Cabecalho {
+
+  topo = "Loja Angular 2"
+
+}
